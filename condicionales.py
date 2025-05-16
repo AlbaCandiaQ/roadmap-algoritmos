2e3e3fedbbd49e20050a9d3185ee82565002dd2b
Alba Candia <alcanque@gmail.com>
2025-05-16 00:44:21 -0400

    condicionales

---
+++ condicionales.py
@@ -0,0 +1,11 @@
+num = int(input("Ingrese un número"))
+if num > 0 :
+    print("El número es positivo")
+elif num == 0 :
+    print("El número es 0")
+else : 
+    print("El número es negativo")
+if num % 2 == 0 :
+    print("El número es par")
+else :
+    print("El número es impar")
